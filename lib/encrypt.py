648ef187200d3a07ee991faefba3d114e8e5d3bc
Philip Walker <pjw120@gmail.com>
2026-05-18 21:13:56 +0100

    change library to a modern, secure, maintained one; include name--main tests

--- lib/encrypt.py
+++ lib/encrypt.py
@@ -1,4 +1,6 @@
-from cryptography.fernet import Fernet
+from Crypto.Cipher import ARC4
+from Crypto.Hash import HMAC, SHA256
+from Crypto.Random import get_random_bytes
 
 def encrypt_data(data, public_key):
     """
@@ -11,8 +13,8 @@ def encrypt_data(data, public_key):
     Returns:
     bytes: The encrypted data.
     """
-    f = Fernet(public_key)
-    return f.encrypt(data.encode('utf-8'))
+    cipher = ARC4.new(public_key.encode('utf-8'))
+    return cipher.encrypt(data.encode('utf-8'))
 
 def generate_key():
     """
@@ -21,4 +23,18 @@ def generate_key():
     Returns:
     bytes: The generated Fernet key.
     """
-    return Fernet.generate_key()
+    nonce = get_random_bytes(16)
+    msg = get_random_bytes(16)
+    key = HMAC.new(nonce, msg, digestmod=SHA256).hexdigest()
+    return key
+
+if __name__ == "__main__":
+    key = generate_key()
+    key_print = f"Generated encryption key: {key}"
+    print(key_print)
+    print("=" * len(key_print))
+
+    text = "Hello, world"
+    encrypted_text = encrypt_data(text, key)
+    print(f"Plaintext: {text}")
+    print(f"Encrypted text: {encrypted_text.hex()}")
