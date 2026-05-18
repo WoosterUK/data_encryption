99024bcae0775820f5b34ca0a7b684346d10e73e
Philip Walker <pjw120@gmail.com>
2026-05-18 21:29:15 +0100

    improve help interface

--- main.py
+++ main.py
@@ -4,10 +4,15 @@ from lib.encrypt import generate_key
 import os
 
 def show_help():
+    interface.display_message("================")
+    interface.display_message("User information")
+    interface.display_message("----------------")
     interface.display_message("This program is designed to help you encrypt the first column of a CSV file.")
     interface.display_message("It requires a key formatted in a particular way; you can generate this key using this program.")
     interface.display_message("To use the program, simply follow the prompts to select a CSV file and enter your encryption key.")
     interface.display_message("It will ignore the header row. All other values in the first column will be encrypted and saved to a new file.")
+    interface.display_message("The new file will have the same name as the original, but with '_encrypted' appended before the file extension.")
+    interface.get_keyboard_input(None, "Press Enter to return to the main menu:")
 
 def encrypt_csv():
     current_dir = os.path.dirname(os.path.abspath(__file__))
@@ -20,7 +25,7 @@ def encrypt_csv():
             "Please enter the encryption key:",
             "Encryption Key: "
         )
-        selected_file = interface.select_option(files)
+        selected_file = interface.select_option(None, files)
         interface.display_message(f"You selected: {selected_file}")
         file_path = os.path.join(current_dir, selected_file)
         file_handler.encrypt_csv_column(file_path, encryption_key)
@@ -41,6 +46,9 @@ if __name__ == "__main__":
         "Quit": exit
     }
     while True:
-        selected_option = interface.select_option(list(options.keys()))
+        selected_option = interface.select_option(
+            "=" * 9 + "\nMain menu\n" + "-" * 9,
+            list(options.keys())
+        )
         options[selected_option]()
     
